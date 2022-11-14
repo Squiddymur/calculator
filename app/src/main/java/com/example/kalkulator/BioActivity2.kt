d0f33d5edc428fc5814603f2b36433429554a825
Squiddymur <gibbrianarifin@gmail.com>
2022-11-14 10:44:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kalkulator/BioActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.kalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BioActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bio2)
+    }
+}
